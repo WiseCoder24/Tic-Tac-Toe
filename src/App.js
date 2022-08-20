f4c5f151048b0270db90c07bbd6c05a5d273e80a
WiseCoder24 <anikethk249@gmail.com>
2022-08-20 09:49:33 +0530

    Coded till creation of the board and its styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
+import {Board} from "./components/Board"
 function App() {
+  const board = ["X","X","X","X","X","X","X","X","X"];
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+    <Board board = {board} onClick={null}/>
+      
+    </>
   );
 }
 
